18cff44491e23ec7c155e9f966b73323accdf309
MW4NG1 <msage3803@gmail.com>
2026-08-15 12:06:35 +0300

    feat: implement Navbar component with navigation links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,1 +1,13 @@
 import { NavLink } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+        <NavLink to ="/" className="nav-link">Home</NavLink>
+        <NavLink to ="/shop" className="nav-link">Shop</NavLink>
+        <NavLink to ="/admin" className="nav-link">Admin Portal</NavLink>
+    </nav>
+  );
+}
+
+export default Navbar;

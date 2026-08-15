5ee6ca882432d57d325dfb234337106463847b57
barack <barackannan00@gmail.com>
2026-08-15 08:56:06 +0300

    feat: add ProductDetails component to display watch information

---
+++ src/pages/ProductDetails.jsx
@@ -0,0 +1,10 @@
+function ProductDetails() {
+  return (
+    <main>
+      <h1>Watch Details</h1>
+      <p>View information about this watch.</p>
+    </main>
+  );
+}
+
+export default ProductDetails;

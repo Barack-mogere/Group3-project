c899e9411ad4241c32b36bac6655c97d75681df9
Abdulmajid <abdullahiabdulmajid848@gmail.com>
2026-08-15 11:23:57 +0300

    feat: implement initial ProductList component with no results handling

---
+++ src/components/ProductList.jsx
@@ -0,0 +1,15 @@
+import ProductCard from './ProductCard';
+
+function ProductList({ watches}) {
+    if watches.length === 0) {
+        return <p className="no-results">No watches Found</p>
+    }
+    // return(
+        // <div className="product-grid">
+             {/* {watches.map(watch => ( */}
+                // <ProductCard key={watch.id} watch={watch} />
+            // ))}
+            {/* </div> */}
+    // );
+// }
+// export default ProductList;

c9f1e5523cb937778653d79b63712fa4fb70f165
MW4NG1 <msage3803@gmail.com>
2026-08-15 12:23:59 +0300

    feat: implement form handling and validation in EditProductForm component

--- src/components/EditProductForm.js
+++ src/components/EditProductForm.js
@@ -24,3 +24,40 @@ function EditProductForm({ watch,onSave }) {
       });
     }
   }, [watch]);
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setFormData((previousData) => ({
+      ...previousData,
+      [name]: value,
+    }));
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    if (
+        !formData.name ||
+        !formData.brand ||
+        !formData.description ||
+        !formData.origin ||
+        !formData.price ||
+        !formData.image
+      ) {
+        setError("Please fill in all fields.");
+        return;
+      }
+
+      setError("");
+
+      const updatedWatch = {
+        ...formData,
+        price: Number(formData.price),
+      };
+
+      try {
+        await onSave(updatedWatch);
+      } catch (error) {
+        setError(error.message);
+      }
+    }
